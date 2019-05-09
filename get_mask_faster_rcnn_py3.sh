1beb90bcbd731c23d2696a96b35ff9ee9170ba63
Seungjun <seungjun45@kaist.ac.kr>
2019-05-09 11:21:44 +0900

    mask_py3

---
+++ get_mask_faster_rcnn_py3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Download integrated zip file (refer for windows, coco api for windows)
+wget -c https://www.dropbox.com/s/xx6k83mj0wzghwm/mask-faster-rcnn-py3.zip?dl=0
+
+
+
+unzip -q mask-faster-rcnn-py3.zip
+rm -rf mask-faster-rcnn-py3.zip
